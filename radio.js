532885eaca9b0bca29af0638cc174db3c8638cd6
J0ZZUE <metanait.211@hotmail.com>
2026-03-17 19:05:45 -0600

    radio.js

--- radio.js
+++ radio.js
@@ -1,28 +1,26 @@
-const MAX = 60;
-
 const aud = new Audio();
 aud.crossOrigin = 'anonymous';
-
-// Pide las estaciones de Radio Browser 
+ 
+// Pide las estaciones a Radio Browser
 async function cargar() {
-    const res  = await fetch(`https://de1.api.radio-browser.info/json/stations?limit=${MAX}&hidebroken=true&order=votes&reverse=true`);
+    const res  = await fetch(`https://de1.api.radio-browser.info/json/stations?hidebroken=true&order=votes&reverse=true`);
     const data = await res.json();
     return data;
 }
-
-// Reproduce el stream.
+ 
+// Reproduce el stream
 function reproducir(url) {
     aud.src = url;
     aud.play().catch(() => console.log('No se pudo conectar'));
 }
-
-// Pausa 
+ 
+// Pausa
 function detener() {
     aud.pause();
     aud.currentTime = 0;
 }
-
-// Subir y bajar el volumen 
+ 
+// Subir y bajar el volumen
 function volumen(val) {
     aud.volume = val / 100;
 }
