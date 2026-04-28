1d6df7e411a670e2e01b5d4814926b0fb3863010
Yeni <139667423+Yanethcr@users.noreply.github.com>
2026-04-28 14:03:29 -0600

    Arreglos
    Ya se arreglo lo de los botones de cambio de estación en la pagina loggeada y en la jam, tambien funciona el buscador de la jam

---
+++ proyectoRadio/radio.js
@@ -0,0 +1,26 @@
+const aud = new Audio();
+aud.crossOrigin = 'anonymous';
+
+// Pide las estaciones a Radio Browser
+async function cargar() {
+    const res = await fetch(`https://de1.api.radio-browser.info/json/stations?hidebroken=true&order=votes&reverse=true`);
+    const data = await res.json();
+    return data;
+}
+
+// Reproduce el stream
+function reproducir(url) {
+    aud.src = url;
+    aud.play().catch(() => console.log('No se pudo conectar'));
+}
+
+// Pausa
+function detener() {
+    aud.pause();
+    aud.currentTime = 0;
+}
+
+// Subir y bajar el volumen
+function volumen(val) {
+    aud.volume = val / 100;
+}
